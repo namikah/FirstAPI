b857c48949b3950093a26a0fd8c5b74c3217478b
Namik Heydarov <heydarovnamiq@gmail.com>
2022-03-17 15:41:39 +0400

    Request from console to Asp.Net API

---
+++ RequestToAPI/Program.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace RequestToAPI
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var httpClient = new HttpClient();
+            var request = await httpClient.GetAsync("https://localhost:44351/api/Students");
+            var response = await request.Content.ReadAsStringAsync();
+
+            var students = JsonConvert.DeserializeObject<List<StudentResponse>>(response);
+
+            foreach (var item in students)
+            {
+                Console.WriteLine($"{item.Id} - {item.Name} - {item.Surname}");
+            }
+        }
+    }
+
+    internal class StudentResponse
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+
+        public int Age { get; set; }
+    }
+}
